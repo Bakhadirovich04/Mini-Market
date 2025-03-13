3e0f781bfe59c9a81170c04457899c5f91e444d0
Bakhadirovich04 <choriyevhusan74@gmail.com>
2025-03-13 15:03:19 +0500

    Seller service

--- src/main/java/com/example/services/SallerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.services;
-
-public class SallerService {
-    public static void service(){
-        System.out.println("Welcome to SallerService");
-    }
-}
---
+++ src/main/java/com/example/services/SellerService.java
@@ -0,0 +1,55 @@
+package com.example.services;
+
+import static com.example.db.DataSource.intScanner;
+
+public class SellerService {
+    public static void service() {
+        while (true) {
+            System.out.print("""
+                    0.exit
+                    1.Add product
+                    2.edit product
+                    3.create shop
+                    4.show shops
+                    5.show history
+                    6.delete history
+                    """);
+            System.out.print("Select a command: ");
+            switch (intScanner.nextInt()){
+                case 0->{return;}
+                case 1->{addProduct();}
+                case 2->{editProduct();}
+                case 3->{createShop();}
+                case 4->{showShops();}
+                case 5->{showHistory();}
+                case 6->{deleteProfile();}
+                default -> {
+                    System.out.println("You cannot do such a thing....!");
+                }
+            }
+        }
+    }
+
+    private static void deleteProfile() {
+    }
+
+    private static void showHistory() {
+
+    }
+
+    private static void showShops() {
+
+    }
+
+    private static void createShop() {
+
+    }
+
+    private static void editProduct() {
+
+    }
+
+    private static void addProduct() {
+
+    }
+}
